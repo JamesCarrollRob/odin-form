e7b7796c3f660cb6bd54bae9c287da65bab21169
JamesCarrollRob <46402913+JamesCarrollRob@users.noreply.github.com>
2023-02-06 18:41:56 -0500

    added some simple validation features

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 
 const subtn = document.getElementById("submitBtn");
+const email = document.getElementById("mail");
 console.log("The file is linked");
 
 
 let firstName = document.getElementById('first-name')
 let lastName = document.getElementById('last-name')
-subtn.addEventListener('click', ()=> {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: firstName.innerText,
-    body: lastName.innerText,
+// subtn.addEventListener('click', ()=> {
+//     fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: firstName.innerText,
+//     body: lastName.innerText,
     
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-  })
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+//   })
 
+ 
+
+  email.addEventListener("input", (event) => {
+    if (email.validity.typeMismatch) {
+      email.setCustomValidity("I am expecting an email address!");
+    } else {
+      email.setCustomValidity("");
+    }
+  });
+  
